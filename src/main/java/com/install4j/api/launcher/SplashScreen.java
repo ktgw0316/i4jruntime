d71b038d059d6bf02c33f8dda2d70dbf2be1cd78
Michel Jung <michel.jung89@gmail.com>
2017-11-14 22:14:42 +0100

    Add SplashScreen

---
+++ src/main/java/com/install4j/api/launcher/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.install4j.api.launcher;
+
+public class SplashScreen {
+
+  public static void hide() throws SplashScreen.ConnectionException {
+
+  }
+
+  public static void writeMessage(String message) throws SplashScreen.ConnectionException {
+
+  }
+
+  public static class ConnectionException extends Exception {
+
+  }
+}
